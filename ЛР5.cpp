7f4be2ac3abd529c6df731030efed1294a045630
Сергій Баранов <onlykomzypc@gmail.com>
2024-10-13 13:38:20 +0300

    початковий файл

---
+++ ЛР5.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    double w, r;
+    cout << "Enter w: ", cin >> w; cout << "Enter n: ", cin >> n;
+    switch (n) {
+    case 1: r = w, cout << r, cout << " kilograms\n";
+        break;
+    case 2: r = w / 1000000, cout << r, cout << " kilograms\n";
+        break;
+    case 3: r = w / 1000, cout << r, cout << " kilograms\n";
+        break;
+    case 4: r = w * 1000, cout << r, cout << " kilograms\n";
+        break;
+    case 5: r = w * 100, cout << r;cout << " kilograms\n";
+        break;  }
+    
+    system("pause");
+    return 0;
+    }
+
