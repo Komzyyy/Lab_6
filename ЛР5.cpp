2cf9dfa04585f9c2bc4b94f6cfa9a56e898b2830
Baranov Sergiy <onlykomzypc@gmail.com>
2024-12-02 14:09:01 +0200

    замінено змінні

--- ЛР5.cpp
+++ ЛР5.cpp
@@ -6,19 +6,19 @@ int main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    int n;
-    double w, r;
-    cout << "Введіть   w: ", cin >> w; cout << "Введіть   n: ", cin >> n;
-    switch (n) {
-    case 1: r = w, cout << r, cout << " кілограм\n";
+    int count;
+    double weight, r;
+    cout << "Введіть   weight: ", cin >> weight; cout << "Введіть   count: ", cin >> count;
+    switch (count) {
+    case 1: r = weight, cout << r, cout << " кілограм\n";
         break;
-    case 2: r = w / 1000000, cout << r, cout << " кілограм\n";
+    case 2: r = weight / 1000000, cout << r, cout << " кілограм\n";
         break;
-    case 3: r = w / 1000, cout << r, cout << " кілограм\n\n";
+    case 3: r = weight / 1000, cout << r, cout << " кілограм\n\n";
         break;
-    case 4: r = w * 1000, cout << r, cout << " кілограм\n\n";
+    case 4: r = weight * 1000, cout << r, cout << " кілограм\n\n";
         break;
-    case 5: r = w * 100, cout << r;cout << " кілограм\n\n";
+    case 5: r = weight * 100, cout << r;cout << " кілограм\n\n";
         break;  }
     
     system("pause");
